431f95507495d5b482d4da2500e093690d9d0e33
github username <github email address>
2020-10-14 20:54:41 -0400

    controller actions

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,14 @@
 class CharactersController < ApplicationController 
+    def index 
+    end 
+
+    def show
+    end 
+
+    def destroy 
+    end 
+
+    private
+    def set_character
+    end 
 end 
