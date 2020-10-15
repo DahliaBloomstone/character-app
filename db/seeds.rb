13d8c6ea439ed7ae6d23c3c9eb949dc7fe802097
github username <github email address>
2020-10-14 20:52:33 -0400

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+c1 = Character.create(name: "John", story: "Manager")
+c2 = Character.create(name: "Dahlia", story: "Artist")
+c3 = Character.create(name: "Jamie", story: "Mom")
+c4 = Character.create(name: "Jackie", story: "Engineer")
+c5 = Character.create(name: "Jones", story: "Painter")
+c6 = Character.create(name: "Jimmy", story: "Cook")
+c7 = Character.create(name: "Dobbie", story: "Baker")
+c8 = Character.create(name: "Erin", story: "Academic")
+c9 = Character.create(name: "Michael", story: "Surgeon")
+c10 = Character.create(name: "Toby", story: "Doctor")
+c11 = Character.create(name: "Alice", story: "Nothing")
