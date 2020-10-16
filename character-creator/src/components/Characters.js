fcce82cde97725ab3d8c1c518cb439f17af202fc
github username <github email address>
2020-10-15 23:12:25 -0400

    character component

---
+++ character-creator/src/components/Characters.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './Characters.css';
+import CharacterCard from '../components/CharacterCard';
+import CharacterForm from './CharacterForm';
+import { getCharacters } from '../actions/characters';
+
+class Characters extends Component{
+
+  componentDidMount() {
+    this.props.getCharacters()
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="CharactersContainer">
+          <h1>Characters</h1>
+          {this.props.characters.map(character =>
+            <CharacterCard
+              key={character.id}
+              character={character}
+            />
+          )}
+        </div>
+        <CharacterForm />
+      </div>
+    )
+  }
+
+};
+
+const mapStateToProps = (state) => {
+  return ({
+    characters: state.characters
+  })
+};
+
+export default connect(mapStateToProps, { getCharacters })(Characters);
