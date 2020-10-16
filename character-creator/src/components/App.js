07ae153a50fb44b9ede18331d00f91382599655a
github username <github email address>
2020-10-15 20:28:43 -0400

    export default reducer

--- character-creator/src/components/App.js
+++ character-creator/src/components/App.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react'
-
+import React from "react"
 import './App.css'
 
-class App extends Component {
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import HelloWorld from './HelloWorld'
+
+class App extends React.Component {
     render(){
         return(
+            <BrowserRouter>
+            <Switch>
             <div className="container app-container">
                 {this.props.children}
             </div>
+            </Switch>
+            </BrowserRouter>
+            
         )
     }
 }
--- character-creator/src/reducers/charactersReducer.js
+++ character-creator/src/reducers/charactersReducer.js
@@ -2,14 +2,19 @@ import _ from 'lodash'
 import { FETCH_CHARACTERS, FETCH_SINGLE_CHARACTER, DELETE_CHARACTER } from '../actions'
 
 export default function(state = {}, action){
+    
     switch(action.type){
         case DELETE_CHARACTER:
             return _.omit(state, action.payload)
-        case FETCH_CHARACTERS:
+       
+            case FETCH_CHARACTERS:
             return _.mapKeys(action.payload.data, "id")
-        case FETCH_SINGLE_CHARACTER:
+       
+            case FETCH_SINGLE_CHARACTER:
             return {...state, [action.payload.data.id]: action.payload.data}
-        default:
-            return state
+        d
+        efault:
+            return state;
     }
 }
+export default charactersReducer;
