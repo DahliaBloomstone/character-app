9c288beb207a4aec09b967afc3fa61d42ba0e807
github username <github email address>
2020-10-15 21:53:56 -0400

    fixed controller actions

--- api/app/controllers/api/characters_controller.rb
+++ api/app/controllers/api/characters_controller.rb
@@ -1,47 +1,46 @@
 class CharactersController < ApplicationController 
   before_action :current_character, only: [:show, :update, :destroy]
 
-    def index 
-        @characters = Character.all 
-        render json: @characters, status: 200 
-    end 
+  def index
+    render json: Character.all
+  end
 
-    def show
-       # @character = Character.find(params[:id]) 
-        render json: @character, status: 200 
-    end 
+  def show
+    render json: @character
+  end
 
-    def create
-        @character = Character.new(character_params)
-    
-        if @character.save
-          render json: @character, status: :created, location: @character
-        else
-          render json: @character.errors, status: :unprocessable_entity
-        end
-      end
-    
-      # PATCH/PUT /characters/1
-      def update
-        if @character.update(character_params)
-          render json: @character
-        else
-          render json: @character.errors, status: :unprocessable_entity
-        end
-      end
-    
-      # DELETE /characters/1
-      def destroy
-        @character.destroy
-      end
-    
-      private
-        def set_character
-          @character = Character.find(params[:id])
-        end
-    
-        def character_params
-          params.require(:character).permit(:name, :story)
-        end
+  def create
+    character = Character.new(character_params)
+    if character.save
+      render json: character
+    else
+      render json: { message: character.errors}, status: 400
     end
-    
+  end
+
+  def update
+    if @character.update(character_params)
+      render json: @character
+    else
+      render json: { message: @character.errors }, status: 400
+    end
+  end
+
+  def destroy
+    if @character.destroy
+      render status: 204
+    else
+      render json: { message: @character.errors }, status: 400
+    end
+  end
+
+  private
+    def character_params
+      params.require(:character).permit(:name, :location, :image_url, :date, :breed, :like)
+    end
+
+    def current_character
+      @character = Character.find_by(id: params[:id])
+    end
+
+end
