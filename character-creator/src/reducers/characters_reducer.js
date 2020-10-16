2ad76ca5a1326a8a53aede34771009dc96f3b9cc
github username <github email address>
2020-10-15 20:15:36 -0400

    added character reducer

---
+++ character-creator/src/reducers/characters_reducer.js
@@ -0,0 +1,15 @@
+import _ from 'lodash'
+import { FETCH_CHARACTERS, FETCH_SINGLE_CHARACTER, DELETE_CHARACTER } from '../actions'
+
+export default function(state = {}, action){
+    switch(action.type){
+        case DELETE_CHARACTER:
+            return _.omit(state, action.payload)
+        case FETCH_CHARACTERS:
+            return _.mapKeys(action.payload.data, "id")
+        case FETCH_SINGLE_CHARACTER:
+            return {...state, [action.payload.data.id]: action.payload.data}
+        default:
+            return state
+    }
+}
