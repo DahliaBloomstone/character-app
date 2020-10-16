8664301dbb3af8da66b97b4a920ec8a4dab02fdc
github username <github email address>
2020-10-15 23:09:03 -0400

    character component handle changes

---
+++ character-creator/src/components/CharacterForm.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { updateCharacterFormData } from '../actions/CharacterForm';
+import { createCharacter } from '../actions/characters';
+
+class CharacterForm extends Component {
+
+  handleOnChange = event => {
+    const { name, value } = event.target;
+    const currentCharacterFormData = Object.assign({}, this.props.characterFormData, {
+      [name]: value
+    })
+    this.props.updateCharacterFormData(currentCharacterFormData)
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault();
+    this.props.createCharacter(this.props.CharacterFormData)
+  }
+
+  render() {
+    const { name, location, image_url, date, background_info } = this.props.characterFormData;
+    return(
+      <div className="CharacterForm">
+        <h3>Add a new Character:</h3>
+        <form onSubmit={this.handleOnSubmit}>
+          <div className="FormField">
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              name="name"
+              onChange={this.handleOnChange}
+              value={name}
+            />
+          </div>
+          <div className="FormField">
+          <label htmlFor="location">Location:</label>
+            <input
+              type="text"
+              name="location"
+              onChange={this.handleOnChange}
+              value={location}
+            />
+          </div>
+          <div className="FormField">
+            <label htmlFor="image_url">Image URL:</label>
+            <input
+              type="text"
+              name="image_url"
+              onChange={this.handleOnChange}
+              value={image_url}
+            />
+          </div>
+          <div className="FormField">
+            <label htmlFor="date">Date:</label>
+            <input
+              type="text"
+              name="date"
+              onChange={this.handleOnChange}
+              value={date}
+            />
+          </div>
+          <div className="FormField">
+            <label htmlFor="background_info">Background Info:</label>
+            <input
+              type="text"
+              name="background_info"
+              onChange={this.handleOnChange}
+              value={background_info}
+            />
+          </div>
+          <button type="submit">Add</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    characterFormData: state.characterFormData
+  }
+}
+
+export default connect(mapStateToProps, { updateCharacterFormData, createCharacter })(CharacterForm);
