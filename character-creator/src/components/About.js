ff9aa1186e6a12e9b889ff391dacd30c4a4d8d2d
github username <github email address>
2020-10-15 23:35:16 -0400

    added about component

---
+++ character-creator/src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className='About'>
+      <h1>About Character Creator</h1>
+      <p>Character Creator is a React/Redux frontend on a Rails backend app! <a href="https://github.com/DahliaBloomstone/react-redux-app/tree/main/character-creator/src/components" target="_blank" rel="noopener noreferrer">Visit</a> the repo to learn more.</p>
+      <p> Also check out: https://github.com/DahliaBloomstone/Character-creator </p>
+    </div>
+  )
+}
+
+export default About;
